9d7e551b0dc6a92d6f163c7924a0efea60334c56
Lizzy Tong <lizzy.tong33@gmail.com>
2019-08-12 17:53:58 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,14 +9,30 @@ def map(source_array)
   return new_array
 end
 
-def reduce(source_array, starting_point = 0)
-  i = 0
+def reduce(source_array, starting_point = nil)
+#if we have a starting_point we set a new variable as the starting_point like we
+#did with the long reduction
+  if starting_point
+    reduction = starting_point
+    i = 0
+  else
+    #DEFAULT STATEMENT
+    reduction = source_array[0]
+    i = 1
+  end
   while i < source_array.length do
-    reduction = yield(source_array[i], starting_point)
+    #for yield: reduction must be the first variable it takes because
+    # it is the variable that changes with each loop
+    #memo is the sum(accumulation/reduction) - why reduction is first
+    #n is the new number(element) taken from the array
+    #for the DEFAULT we start with the reduction = source_array[0]
+    #and source_array[1]
+    #  - vs -
+    #if starting_point we start loop with reduction = 100(given starting_point)
+    #and source_array[0]
+    reduction = yield(reduction, source_array[i])
     i += 1
   end
-return false if reduction == nil
-return reduction + 3 if reduction.class == Integer
-return reduction
+reduction
 end
-#tweaked the code to pass test so that I can look at the solution to figure out corect way to do reduction
+#see notes on test page to understand looping
