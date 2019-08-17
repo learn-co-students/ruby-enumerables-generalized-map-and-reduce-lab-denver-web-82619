7f6f4e8e16fbf63b0ad8983eefe45d03f5df2bc8
Arlene George <aemg@live.com>
2019-08-17 22:12:14 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,27 @@
-# Your Code Here
+def map(array)
+  new = []
+  i = 0
+  while i <array.length do
+    new.push(yield(array[i]))
+    i += 1
+  end
+  return new
+end
+
+
+def reduce(array, starting_point=nil)
+  if starting_point
+    a = 0
+    answer = starting_point
+  else
+    a = 1
+    answer = array[0]
+  end
+    while a < array.length do	 
+    answer = yield(answer, array[a])
+      a += 1
+ end
+    return answer
+end
+
+  
