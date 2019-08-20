f756a04a6bee63f34b9c56ebf13cfd4a0d070817
Andrew Thiessen <squaids449@gmail.com>
2019-08-20 03:39:51 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,27 @@
 # Your Code Here
+  
+  
+def map(array)
+  new = []
+  i = 0
+  while i < array.length do
+    new.push(yield(array[i]))
+    i += 1
+  end
+  return new
+end
+
+def reduce(array, sv=nil)
+  if sv
+    num = sv
+    i = 0
+  else
+    num = array[0]
+    i = 1
+  end
+  while i < array.length do
+    num = yield(num, array[i])
+    i += 1
+  end
+  num
+end
