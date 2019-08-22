30c449a2adbbf976bec4a1e443c9e1888c03e477
Aaron Martinez <aaronmarti17@gmail.com>
2019-08-22 07:20:39 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,19 @@
 # Your Code Here
+def map(source_array)
+  new_array = []
+  source_array.each do |value|
+    new_array.push(yield(value))
+  end
+  new_array
+end
+
+def reduce(source_array, starting_point = 0)
+  total = starting_point
+  for it in source_array do
+    total = yield(total, it)
+  end
+  if total == 0
+    return true
+  end
+  total
+end
