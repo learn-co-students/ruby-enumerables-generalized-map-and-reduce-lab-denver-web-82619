b4547d672eb5d8e3c5f543f54d1ef4c3f0b1164e
Colin Alexander <chalexander@me.com>
2019-08-22 16:54:37 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,32 @@
-# Your Code Here
+def map(array)
+  count = 0
+  result = []
+  while array[count] do
+    result[count] = yield(array[count])
+    count += 1
+  end
+  return result
+end
+
+def reduce(array, starting_value = 0)
+  count = 0
+  result = starting_value
+  if result == 0 
+    result = array[count]
+    count += 1
+  end
+  while count < array.length do
+      result = yield(result, array[count])
+      count += 1
+  end
+  return result
+end
+
+
+
+
+
+
+
+
+
